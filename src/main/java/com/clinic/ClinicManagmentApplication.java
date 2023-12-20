560128759e0d1fca1647776e3fc661c94f76b413
ameni-selmi <ameniselmi456@gmail.com>
2023-12-21 08:35:50 +1300

    ameni commit

--- src/main/java/com/clinic/ClinicManagmentApplication.java
+++ src/main/java/com/clinic/ClinicManagmentApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ClinicManagmentApplication {
 
 	public static void main(String[] args) {
+		System.out.print("hello");
 		SpringApplication.run(ClinicManagmentApplication.class, args);
 	}
-
+//ameni
+	
 }
