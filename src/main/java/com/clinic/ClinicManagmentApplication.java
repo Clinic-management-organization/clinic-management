24240f342f7200ca0a6de3c497665d355d886b9e
ameni-selmi <ameniselmi456@gmail.com>
2023-12-26 06:46:12 +1300

    commit changes

--- src/main/java/com/clinic/ClinicManagmentApplication.java
+++ src/main/java/com/clinic/ClinicManagmentApplication.java
@@ -1,14 +1,18 @@
 package com.clinic;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import com.clinic.dao.DossierMedicalDAO;
 import com.clinic.dao.MedecinDAO;
 import com.clinic.dao.PatientDAO;
 import com.clinic.dao.RendezVousDAO;
+import com.clinic.entity.DossierMedical;
 import com.clinic.entity.Medecin;
 import com.clinic.entity.Patient;
 import com.clinic.entity.RendezVous;
@@ -16,7 +20,7 @@ import com.clinic.entity.Enum.EtatRDV;
 import com.clinic.entity.Enum.GenderType;
 import com.clinic.entity.Enum.RoleType;
 import com.clinic.entity.Enum.SpecialiteType;
-
+import java.time.LocalDateTime;
 @SpringBootApplication
 public class ClinicManagmentApplication {	
 	public static void main(String[] args) {
@@ -98,9 +102,29 @@ public class ClinicManagmentApplication {
 		       rdv3.setEtatRendezVous(EtatRDV.RAPPORTEE);
 		       rdv3.setMedecin(medecin2);
 		       rdv3.setPatient(patient2);
+		       
+		DossierMedical dm1 = new DossierMedical() ;
+		       		dm1.setConsultations(null);
+		       		dm1.setDateCreation(LocalDateTime.now());
+		       		dm1.setDateMiseAJour(LocalDateTime.now());
+		       		dm1.setObservation("ahla dossier");
+		       		List<RendezVous> rdvs = new ArrayList<>();
+		       		rdvs.add(rdv);
+		       		rdvs.add(rdv2);
+		       		dm1.setRdvs(rdvs);
+		DossierMedical dm2 = new DossierMedical() ;
+		       		dm2.setConsultations(null);
+		       		dm2.setDateCreation(LocalDateTime.now());
+		       		dm2.setDateMiseAJour(LocalDateTime.now());
+		       		dm2.setObservation("ahla dossier");
+		       		List<RendezVous> rdvs2 = new ArrayList<>();
+		       		rdvs.add(rdv3);
+		       		rdvs.add(rdv2);
+		       		dm1.setRdvs(rdvs2);		       		
         MedecinDAO medecinDAO=ctx.getBean(MedecinDAO.class);
         PatientDAO patientDAO=ctx.getBean(PatientDAO.class);
         RendezVousDAO rdvDAO=ctx.getBean(RendezVousDAO.class);
+        DossierMedicalDAO dmDAO=ctx.getBean(DossierMedicalDAO.class);
         
 		medecinDAO.save(medecin1);
         medecinDAO.save(medecin2);
@@ -109,6 +133,8 @@ public class ClinicManagmentApplication {
         rdvDAO.save(rdv); 
         rdvDAO.save(rdv2); 
         rdvDAO.save(rdv3);
+        dmDAO.save(dm1) ;
+        dmDAO.save(dm2) ;
         
 	}	
 	
--- src/main/java/com/clinic/dao/DossierMedicalDAO.java
+++ src/main/java/com/clinic/dao/DossierMedicalDAO.java
@@ -3,10 +3,16 @@ package com.clinic.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.clinic.entity.DossierMedical;
 
 
 public interface DossierMedicalDAO extends JpaRepository<DossierMedical, Long>{
-	List<DossierMedical> findByPatientId(Long id);
+	 @Query("SELECT dm FROM DossierMedical dm " +
+	            "JOIN dm.rdvs rdv " +
+	            "JOIN rdv.patient p " +
+	            "WHERE dm.id = rdv.dossierMedical.id AND p.id = :patientId")
+	    List<DossierMedical> findDossierMedicalsByPatientId(@Param("patientId") Long patientId);
 }
