3fef127590fc8241b2b416be03eab3edd35d9463
Nourchene <hamritanourchene@gmail.com>
2024-01-17 20:16:56 +0100

    add data

--- src/main/java/com/clinic/ClinicManagmentApplication.java
+++ src/main/java/com/clinic/ClinicManagmentApplication.java
@@ -72,6 +72,7 @@ public class ClinicManagmentApplication {
 		        medecin2.setSpecialite(SpecialiteType.GYNECOLOGIE);
 		        medecin2.setHoraires(null);  // Set the list of horaires
 		        medecin2.setRDVs(null);   // Set the list of RDVs
+		        
         // Create and save sample Patient objects
 		Patient patient1 = new Patient() ;
 				patient1.setNom("Ameni");
@@ -97,6 +98,9 @@ public class ClinicManagmentApplication {
         		patient2.setRole(RoleType.MEDECIN);
 				patient2.setSituationFamilliale("married");
         		patient2.setRDVs(null);   // Set the list of RDVs
+        		
+        		
+        		
 
         RendezVous rdv = new RendezVous();
         		rdv.setDateRendezVous(new Date());
@@ -114,6 +118,20 @@ public class ClinicManagmentApplication {
 		       rdv3.setMedecin(medecin2);
 		       rdv3.setPatient(patient2);
 		       
+		       
+		       RendezVous rdv4 = new RendezVous();
+		       rdv4.setDateRendezVous(new Date());
+		       rdv4.setEtatRendezVous(EtatRDV.RAPPORTEE);
+		       rdv4.setMedecin(medecin1);
+		       rdv4.setPatient(patient1);
+
+		       RendezVous rdv5 = new RendezVous();
+		       rdv5.setDateRendezVous(new Date());
+		       rdv5.setEtatRendezVous(EtatRDV.RAPPORTEE);
+		       rdv5.setMedecin(medecin2);
+		       rdv5.setPatient(patient2);
+
+		       
        DossierMedical dm1 = new DossierMedical();
 		       dm1.setConsultations(null);
 		       dm1.setDateCreation(new Date());
@@ -125,27 +143,137 @@ public class ClinicManagmentApplication {
 		       dm2.setDateCreation(new Date());
 		       dm2.setDateMiseAJour(new Date());
 		       dm2.setObservation("Test dossier 2");
+		       DossierMedical dossierMedical3 = new DossierMedical();
+		       dossierMedical3.setConsultations(null);
+		       dossierMedical3.setDateCreation(new Date());
+		       dossierMedical3.setDateMiseAJour(new Date());
+		       dossierMedical3.setObservation("Test dossier 3");
+
+
        	Consultation c1 = new Consultation() ;
 	  			c1.setDossierMedical(dm1);
 	  			c1.setPrix(50);
 	  			c1.setDateCreation(new Date());
 	  			c1.setSynthese("consultation terminé");
+	  			
+	  			Consultation consultation2 = new Consultation();
+	  			consultation2.setDossierMedical(dm1);
+	  			consultation2.setPrix(60);
+	  			consultation2.setDateCreation(new Date());
+	  			consultation2.setSynthese("Consultation en cours");
+
+	  			Consultation consultation3 = new Consultation();
+	  			consultation3.setDossierMedical(dm2);
+	  			consultation3.setPrix(70);
+	  			consultation3.setDateCreation(new Date());
+	  			consultation3.setSynthese("Consultation prévue");
+
 	  Traitement t1 =  new Traitement() ;
 				t1.setConsultation(c1);
 				t1.setDosage("3 doses");
 				t1.setMedicament("Panadol");
 				t1.setStartDate(LocalDate.now());
 				t1.setEndDate(LocalDate.now());
+				// Création et sauvegarde d'autres objets Traitement
+				Traitement traitement2 = new Traitement();
+				traitement2.setConsultation(consultation2);
+				traitement2.setDosage("2 doses");
+				traitement2.setMedicament("Aspirine");
+				traitement2.setStartDate(LocalDate.now());
+				traitement2.setEndDate(LocalDate.now().plusDays(7));
+
+				Traitement traitement3 = new Traitement();
+				traitement3.setConsultation(consultation3);
+				traitement3.setDosage("1 dose");
+				traitement3.setMedicament("Vitamine C");
+				traitement3.setStartDate(LocalDate.now());
+				traitement3.setEndDate(LocalDate.now().plusDays(5));
+
 	  			
 	  Diagnostic d1 =  new Diagnostic() ;
 				d1.setConsultation(c1);
 				d1.setDescription("Radiologie");
 				
+				
+				// Création et sauvegarde d'autres objets Diagnostic
+				Diagnostic diagnostic2 = new Diagnostic();
+				diagnostic2.setConsultation(consultation2);
+				diagnostic2.setDescription("Échographie");
+
+				Diagnostic diagnostic3 = new Diagnostic();
+				diagnostic3.setConsultation(consultation3);
+				diagnostic3.setDescription("Bilan sanguin");
+
+				
 	  			
 		       rdv.setDossierMedical(dm2);
 		       rdv2.setDossierMedical(dm1);
 		       rdv3.setDossierMedical(dm1);
-		       
+		    // Création et sauvegarde d'autres objets Medecin
+		       Medecin medecin3 = new Medecin();
+		       medecin3.setNom("Marie");
+		       medecin3.setPrenom("Dupont");
+		       medecin3.setDateNaissance(new Date());
+		       medecin3.setSexe(GenderType.F);
+		       medecin3.setAdresse("789 Rue Principale");
+		       medecin3.setTel("987654321");
+		       medecin3.setEmail("marie.dupont@example.com");
+		       medecin3.setRole(RoleType.MEDECIN);
+		       medecin3.setSpecialite(SpecialiteType.DERMATOLOGIE);
+
+		       // Création et sauvegarde d'autres objets Patient
+		       Patient patient3 = new Patient();
+		       patient3.setNom("Jean");
+		       patient3.setPrenom("Martin");
+		       patient3.setDateNaissance(new Date());
+		       patient3.setSexe(GenderType.H);
+		       patient3.setAdresse("456 Rue Secondaire");
+		       patient3.setTel("123456789");
+		       patient3.setEmail("jean.martin@example.com");
+		       patient3.setRole(RoleType.PATIENT);
+		       patient3.setSituationFamilliale("divorced");
+
+		       // Création et sauvegarde d'autres objets DossierMedical
+		       DossierMedical dossierMedical4 = new DossierMedical();
+		       dossierMedical4.setConsultations(null);
+		       dossierMedical4.setDateCreation(new Date());
+		       dossierMedical4.setDateMiseAJour(new Date());
+		       dossierMedical4.setObservation("Test dossier 4");
+
+		       // Création et sauvegarde d'autres objets Medecin
+		       Medecin medecin4 = new Medecin();
+		       medecin4.setNom("Philippe");
+		       medecin4.setPrenom("Lefevre");
+		       medecin4.setDateNaissance(new Date());
+		       medecin4.setSexe(GenderType.H);
+		       medecin4.setAdresse("987 Rue Tertiaire");
+		       medecin4.setTel("123456789");
+		       medecin4.setEmail("philippe.lefevre@example.com");
+		       medecin4.setRole(RoleType.MEDECIN);
+		       medecin4.setSpecialite(SpecialiteType.DERMATOLOGIE);
+
+		       // Création et sauvegarde d'autres objets Patient
+		       Patient patient4 = new Patient();
+		       patient4.setNom("Sophie");
+		       patient4.setPrenom("Dufresne");
+		       patient4.setDateNaissance(new Date());
+		       patient4.setSexe(GenderType.F);
+		       patient4.setAdresse("789 Rue Quaternaire");
+		       patient4.setTel("987654321");
+		       patient4.setEmail("sophie.dufresne@example.com");
+		       patient4.setRole(RoleType.PATIENT);
+		       patient4.setSituationFamilliale("single");
+
+		       // Création et sauvegarde d'autres objets DossierMedical
+		       DossierMedical dossierMedical5 = new DossierMedical();
+		       dossierMedical5.setConsultations(null);
+		       dossierMedical5.setDateCreation(new Date());
+		       dossierMedical5.setDateMiseAJour(new Date());
+		       dossierMedical5.setObservation("Test dossier 5");
+
+		       // Sauvegarde des objets en utilisant les DAOs
+		     
+       
       
         MedecinDAO medecinDAO=ctx.getBean(MedecinDAO.class);
         PatientDAO patientDAO=ctx.getBean(PatientDAO.class);
@@ -154,19 +282,42 @@ public class ClinicManagmentApplication {
         DiagnosticDAO dDAO=ctx.getBean(DiagnosticDAO.class);
         DossierMedicalDAO dmDAO=ctx.getBean(DossierMedicalDAO.class);
         ConsultationDAO cDAO=ctx.getBean(ConsultationDAO.class);
+        
+        
+    
       
 		medecinDAO.save(medecin1);
         medecinDAO.save(medecin2);
         patientDAO.save(patient1);
         patientDAO.save(patient2);
+        medecinDAO.save(medecin3);
+	    medecinDAO.save(medecin4);
+	    patientDAO.save(patient3);
+	    patientDAO.save(patient4);
+	    dmDAO.save(dossierMedical4);
+	    dmDAO.save(dossierMedical5);
         dmDAO.save(dm1) ;
         dmDAO.save(dm2) ;
         rdvDAO.save(rdv); 
         rdvDAO.save(rdv2); 
         rdvDAO.save(rdv3);
+       
         cDAO.save(c1);
+     
         tDAO.save(t1);
+       
         dDAO.save(d1);
+        rdvDAO.save(rdv4);
+        rdvDAO.save(rdv5);
+        cDAO.save(consultation2);
+        cDAO.save(consultation3);
+        tDAO.save(traitement2);
+        tDAO.save(traitement3);
+        dDAO.save(diagnostic2);
+        dDAO.save(diagnostic3);
+        
+ 
+       
 	}	
 	@Bean
 	CommandLineRunner run(RoleDAO roleDAO, UserDAO userDAO,PatientDAO patientDAO, PasswordEncoder passwordEncode){
